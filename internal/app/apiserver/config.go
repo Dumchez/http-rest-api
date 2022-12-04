3653cdd7dc9d8425addd7a5c72850b169e120d68
Dumchez <domasp902@gmail.com>
2022-12-04 23:21:06 +0200

    4

--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -1,14 +1,18 @@
 package apiserver
 
+import "github.com/Dumchez/http-rest-api/internal/app/store"
+
 type Config struct {
 	BindAddr string `toml:"bind_addr"`
 	LogLevel string `toml:"log_level"`
+	Store    *store.Config
 }
 
 func NewConfig() *Config {
 	return &Config{
 		BindAddr: ":8080",
 		LogLevel: "debug",
+		Store:    store.NewConfig(),
 		//default values
 	}
 
