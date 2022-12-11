9bf5b4a0e000594eb41cd3e18da8d570333c9467
Dumchez <domasp902@gmail.com>
2022-12-11 20:26:46 +0200

    Refactored

--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -1,18 +1,15 @@
 package apiserver
 
-import "github.com/Dumchez/http-rest-api/internal/app/store"
-
 type Config struct {
-	BindAddr string `toml:"bind_addr"`
-	LogLevel string `toml:"log_level"`
-	Store    *store.Config
+	BindAddr    string `toml:"bind_addr"`
+	LogLevel    string `toml:"log_level"`
+	DatabaseURL string `toml:"database_url"`
 }
 
 func NewConfig() *Config {
 	return &Config{
 		BindAddr: ":8080",
 		LogLevel: "debug",
-		Store:    store.NewConfig(),
 		//default values
 	}
 
---
+++ internal/app/store/sqlstore/store_test.go
@@ -0,0 +1,19 @@
+package sqlstore_test
+
+import (
+	"os"
+	"testing"
+)
+
+var (
+	databaseURL string
+)
+
+func TestMain(m *testing.M) {
+	databaseURL = os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		databaseURL = "host=localhost port=5432 user=postgres password=222 dbname=restapi_dev sslmode=disable connect_timeout=10"
+	}
+
+	os.Exit(m.Run())
+}
